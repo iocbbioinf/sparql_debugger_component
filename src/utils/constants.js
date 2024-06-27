2014ca3894ce85fb980dea2288ebb55ff598187b
marek <moos@uochb.cas.cz>
2024-06-27 16:27:20 +0200

    Env Config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,4 @@ export const PENDING_STATE = "IN_PROGRESS";
 export const SUCCESS_STATE = "SUCCESS";
 export const FAILURE_STATE = "ERROR";
 
-export const baseUrl = "http://idsm-debugger-test6.dyn.cloud.e-infra.cz";
+export const baseUrl = process.env.REACT_APP_SERVER_URL ? process.env.REACT_APP_SERVER_URL : "http://idsm-debugger-test6.dyn.cloud.e-infra.cz";
